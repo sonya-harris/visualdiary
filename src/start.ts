ca8d4e1a16b976dc643788e758ede73fe10b52b2
sonyakateharris <sonyakateharris@gmail.com>
2026-06-26 23:51:23 +0000

    fix: enable SPA mode with maskPath for static hosting

--- src/start.ts
+++ src/start.ts
@@ -18,5 +18,9 @@ const errorMiddleware = createMiddleware().server(async ({ next }) => {
 });
 
 export const startInstance = createStart(() => ({
+  spa: {
+    enabled: true,
+    maskPath: "/",
+  },
   requestMiddleware: [errorMiddleware],
 }));
